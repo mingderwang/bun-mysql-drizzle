f3a1b1176f340b2ca20c611b69be37c3d8ef5320
Ming-der Wang <mingderwang@gmail.com>
2023-11-16 23:42:49 +0800

    add seed and exec bun run seed.ts

---
+++ seed.ts
@@ -0,0 +1,20 @@
+import { db } from "./db";
+import * as schema from "./schema";
+
+await db.insert(schema.movies).values([
+  {
+    title: "The Matrix",
+    releaseYear: 1999,
+  },
+  {
+    title: "The Matrix Reloaded",
+    releaseYear: 2003,
+  },
+  {
+    title: "The Matrix Revolutions",
+    releaseYear: 2003,
+  },
+]);
+
+console.log(`Seeding complete.`);
+
