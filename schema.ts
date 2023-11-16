aec51c72f923466469bd7ed904264e5d1fec64d3
Ming-der Wang <mingderwang@gmail.com>
2023-11-16 23:39:57 +0800

    create schema for movie and run bunx drizzle-kit generate:sqlite --schema ./schema.ts

---
+++ schema.ts
@@ -0,0 +1,8 @@
+import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+
+export const movies = sqliteTable("movies", {
+  id: integer("id").primaryKey(),
+  title: text("name"),
+  releaseYear: integer("release_year"),
+});
+
